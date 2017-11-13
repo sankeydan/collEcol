bdb99be6e86fee9d76ad4e8719a4f75566b34fba
sankeydan <dwsankey@gmail.com>
2017-11-13 14:15:26 +0000

    plot_map update: including easier data entry (now as a list) and also ability to change the size of the axis numbers and labels.

--- R/plot_map.R
+++ R/plot_map.R
@@ -1,7 +1,7 @@
 #' plot_map
 #'
 #' A handy and interactive plotter for solo or group GPS tracks. Uses ggmaps to produce images or videos of animal trajectories on a googlemaps background.
-#' @param data data array with dimensions time (rows), longitude / latitude (in that order; columns) & individual (3rd dimension). No third dimension necessary for solo plots.
+#' @param data data frame or list of data frames with dimensions time (rows), longitude / latitude (in that order; columns) & individual (3rd dimension). No third dimension necessary for solo plots.
 #' @param type Solo or Group plots.
 #' @param image.or.vid "image" or "video" (sequence of images) to be produced
 #' @param tail.size animals represented by current position and the previous n positions (video only)
@@ -12,47 +12,81 @@
 #' @param wd Specify the working directory that you would like to save the plots into.
 #' @param col Colour of plotted trajectory (solo only)
 #' @param lwd Thickness of line
+#' @param centre Choose your own centre points for the mapform: c(lon, lat)
 #' @param file.to.folder TRUE if you want to make videos when happy with other params, but for testing videos set to FALSE
 #' @export
 
 
 
-plot_map = function( data,  type = c("solo", "group") , image.or.vid = "video",
+plot_map = function( data,  type = c("solo", "group") , image.or.vid = "image",
                      tail.size = 20, frame.size = 1, wd = getwd(), plot_name = "my_plot" ,
-                     lines.or.points = "lines", col = "red" , lwd = 2, file.to.folder = T,
-                     maptype = "satellite", zoom = 19 ){
+                     lines.or.points = "lines", col = "red" , axis.nums.size = 12, axis.labs.size = 14,
+                     lwd = 2, file.to.folder = T, centre = NA , maptype = "satellite", zoom = 19 ){
 
-  # data = data2
-  # type = "group"
-  # image.or.vid = "video"
-  # tail.size = 20
-  # file.to.folder = T
-  # zoom = 19
-  # frame.size = 5
-  # maptype = "satellite"
+  # library(ggmap)
+  # library(ggplot2)
 
-  stopifnot(length(type) == 1 & length(image.or.vid) == 1)
+  if ( type[1] == "solo"){
 
+    if ( class(data) == "data.frame"){
+      dat = as.data.frame(data)
+      names(dat) = c("lon", "lat")
+      num.of.files = 1
+      lis = F
+    }
 
-  if ( type == "solo"){
+    if ( class(data) == "list"){
+      num.of.files = length(data)
+      dat = as.data.frame(data[[1]])
+      names(dat) = c("lon", "lat")
+      lis = T
+    }
 
+    if ( is.na(centre[1]) ){ # if no centre is specified.
+      centre = c ( (max( dat[ ,1] ) + min( dat [ ,1]) ) /2 , (max( dat[ ,2] ) + min( dat [ ,2]) ) /2 ) # find centre for map
+    }
 
-    data = as.data.frame(data)
-    names(data) = c("lon", "lat") # give the columns names
-
-    centre = c ( (max( data[ ,1] ) + min( data [ ,1]) ) /2 , (max( data[ ,2] ) + min( data [ ,2]) ) /2 ) # find centre for map
 
     map = ggmap::get_map( location = centre,
                           zoom = zoom,
                           maptype = maptype) # load map
     p = ggmap::ggmap(map)
 
-    p = p + ggplot2::geom_path (data= data ,  aes(x=lon, y=lat), color = col , size = lwd)
+    for ( i in 1:num.of.files){
 
+      if ( lis == T){
+        dat = as.data.frame(data[[i]])
+        names(dat) = c("lon", "lat")
+      }
+      p = p + ggplot2::geom_path (data= dat, ggplot2::aes(x=lon, y=lat),
+                                  color = col[i], size = lwd)
+    }
+
+    scale_x_longitude <- function(xmin=-180, xmax=180, step=1, ...) {
+      xbreaks <- seq(xmin,xmax,step)
+      xlabels <- unlist(lapply(xbreaks, function(x) ifelse(x < 0, parse(text=paste0(x,"^o", "*W")), ifelse(x > 0, parse(text=paste0(x,"^o", "*E")),x))))
+      return(scale_x_continuous("Longitude", breaks = xbreaks, labels = xlabels, expand = c(0, 0), ...))
+    }
+
+    scale_y_latitude <- function(ymin=-90, ymax=90, step=0.5, ...) {
+      ybreaks <- seq(ymin,ymax,step)
+      ylabels <- unlist(lapply(ybreaks, function(x) ifelse(x < 0, parse(text=paste0(x,"^o", "*S")), ifelse(x > 0, parse(text=paste0(x,"^o", "*N")),x))))
+      return(scale_y_continuous("Latitude", breaks = ybreaks, labels = ylabels, expand = c(0, 0), ...))
+    }
+
+    # ggmap(m) +
+    #   geom_blank() +
+    #   scale_x_longitude(xmin=-10, xmax=10, step=5) +
+    #   scale_y_latitude(ymin=-10, ymax=10, step=5) +
+    p = p  + ggplot2::xlab("Longitude")+
+    ggplot2::ylab("Latitude") +
+    ggplot2::theme(axis.text= ggplot2::element_text(size= axis.nums.size),
+          axis.title= ggplot2::element_text(size= axis.labs.size ,face="bold"))
     print(p)
   }
 
-  if ( type == "group"){
+
+  if ( type[1] == "group"){
 
     for ( j in seq(tail.size,nrow(data),frame.size)){
 
@@ -97,6 +131,10 @@ plot_map = function( data,  type = c("solo", "group") , image.or.vid = "video",
     }
   }
 }
+
+
+
+
 
 
 
